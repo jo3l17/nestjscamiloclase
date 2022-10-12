86ef4e4bb90da8603af9aabc2e2d53d7a1b6d7a6
jo3l17 <joelvaldezangeles@gmail.com>
2022-10-11 19:13:36 -0500

    feat: adding delete uuuids for tests

--- src/product/product.service.spec.ts
+++ src/product/product.service.spec.ts
@@ -1,11 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from '../common/prisma/prisma.service';
 import { CommonModule } from '../common/common.module';
 import { ProductRequestDto } from './dto/product-request.dto';
 import { ProductService } from './product.service';
 
 describe('ProductService', () => {
   let service: ProductService;
+  let prismaService: PrismaService;
+  const productUuids = [];
 
+  afterAll(async () => {
+    await prismaService.product.deleteMany({
+      where: {
+        AND: [...productUuids],
+      },
+    });
+  });
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [ProductService],
@@ -13,6 +23,7 @@ describe('ProductService', () => {
     }).compile();
 
     service = module.get<ProductService>(ProductService);
+    prismaService = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
@@ -20,9 +31,10 @@ describe('ProductService', () => {
   });
   it('should return product.name + 1', async () => {
     const product: ProductRequestDto = {
-      name: 'leche',
+      name: 'pollo',
     };
     const response = await service.createProduct(product);
+    productUuids.push({ uuid: response.uuid });
     expect(response.name).toBe(product.name + ' + 1');
   });
 });
